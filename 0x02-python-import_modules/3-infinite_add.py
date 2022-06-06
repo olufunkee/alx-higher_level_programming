ccada138d5a31d2203d55570e19c3a76ae114268
olufunkee <volufunke610@gmail.com>
2022-06-05 20:31:04 -0700

    Add infinity numbers

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,14 @@
 #!/usr/bin/bash
-import sys
-n = len(sys.argv)
+if __name__ == "__main__":
+    import sys
 
-sum = 0
-for i in range(1, n):
-    sum += int(sys.argv[i])
+    count = len(sys.argv)
+    summ = 0
 
-print("{}".format(sum))
+    if count == 1:
+        print ("{}".format(summ))
+    else:
+        for i in range(1, count):
+            summ += int(sys.argv[i])
+
+        print("{}".format(summ))
